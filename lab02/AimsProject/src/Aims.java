2c56aa2074e0b7f234825fb730c88aad24fdfbe8
minh <minhtrinhvan4405@gmail.com>
2026-04-03 14:00:45 +0700

    11,12,13

--- lab02/AimsProject/src/Aims.java
+++ lab02/AimsProject/src/Aims.java
@@ -1,5 +1,16 @@
 public class Aims {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Cart anOrder= new Cart();
+        DigitalVideoDisc dvd1= new DigitalVideoDisc("The Lion King","Animation","Roger Allers",87,19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+        DigitalVideoDisc dvd2= new DigitalVideoDisc("Star Wars","Science Fiction","George Lucas",87,24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+        DigitalVideoDisc dvd3= new DigitalVideoDisc("Aladdin","Animation",18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+        anOrder.printCost();
+        anOrder.removeDigitalVideoDisc(dvd1);
+        anOrder.printCost();
+        DigitalVideoDisc dvd4=new DigitalVideoDisc("bubu","Animation",20.99f);
+        anOrder.removeDigitalVideoDisc(dvd4);
     }
 }
